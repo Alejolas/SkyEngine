aa4db363cbc176a1d6cebed5010e2c308afb6f5f
Alejandro <aalejo87@gmail.com>
2023-05-29 22:43:17 -0500

    Updated Code

--- src/Memory.h
+++ src/Memory.h
@@ -1,12 +1,9 @@
 #pragma once
+
 #include <iostream>
 #include <string>
 #include <Windows.h>
 #include <TlHelp32.h>
-
-using std::cout;
-using std::endl;
-using std::string;
 
 // datatype for a module in memory (dll, regular exe) 
 struct module
@@ -22,86 +19,121 @@ public:
 	HANDLE TargetProcess; // for target process
 	DWORD  TargetId;      // for target process
 
-	// For getting a handle to a process
+	// Opens a handle to a process with the given name.
+	// Returns nullptr and prints an error message on failure.
 	HANDLE GetProcess(const char* processName)
-	{		
+	{
 		WCHAR wProcessName[MAX_PATH] = { 0 };
 		mbstowcs(wProcessName, processName, strlen(processName));
 
-		HANDLE handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+		HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+		if (snapshot == INVALID_HANDLE_VALUE) {
+			std::cerr << "Failed to create snapshot: " << GetLastError() << std::endl;
+			return nullptr;
+		}
+
 		PROCESSENTRY32 entry;
 		entry.dwSize = sizeof(entry);
-		
-		do
+
+		do {
 			if (!_wcsicmp(entry.szExeFile, wProcessName)) {
 				TargetId = entry.th32ProcessID;
-				CloseHandle(handle);
+				CloseHandle(snapshot);
+
 				TargetProcess = OpenProcess(PROCESS_ALL_ACCESS, false, TargetId);
+				if (TargetProcess == NULL) {
+					std::cerr << "Failed to open process: " << GetLastError() << std::endl;
+					return nullptr;
+				}
+
 				return TargetProcess;
 			}
-		while (Process32Next(handle, &entry));
+		} while (Process32Next(snapshot, &entry));
 
-		return false;
+		CloseHandle(snapshot);
+		return nullptr;
 	}
 
-	// For getting information about the executing module
+	// Gets information about the given module in the target process.
+	// Returns a module with null pointers and prints an error message on failure.
 	module GetModule(const char* moduleName) {
 		WCHAR wModuleName[MAX_PATH] = { 0 };
 		mbstowcs(wModuleName, moduleName, strlen(moduleName));
 
-		HANDLE hmodule = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, TargetId);
+		HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, TargetId);
+		if (snapshot == INVALID_HANDLE_VALUE) {
+			std::cerr << "Failed to create snapshot: " << GetLastError() << std::endl;
+			module mod = { 0, 0 };
+			return mod;
+		}
+
 		MODULEENTRY32 mEntry;
 		mEntry.dwSize = sizeof(mEntry);
 
-		do 
-		{
+		do {
 			if (!_wcsicmp(mEntry.szModule, wModuleName)) {
-				CloseHandle(hmodule);
+				CloseHandle(snapshot);
 
 				TargetModule = { (DWORD_PTR)mEntry.hModule, mEntry.modBaseSize };
 				return TargetModule;
 			}
-		} while (Module32Next(hmodule, &mEntry));
+		} while (Module32Next(snapshot, &mEntry));
 
-		module mod = { (DWORD_PTR)false, (DWORD)false };
+		CloseHandle(snapshot);
+		module mod = { 0, 0 };
 		return mod;
 	}
 
-	// Basic WPM wrapper, easier to use.
-	template <typename var>
-	bool WriteMemory(DWORD_PTR Address, var Value) {
-		return WriteProcessMemory(TargetProcess, (LPVOID)Address, &Value, sizeof(var), 0);
+	// Writes a value to a memory address in the target process.
+	// Returns true on success, false on failure.
+	template <typename T>
+	bool WriteMemory(DWORD_PTR address, T value) {
+		if (!WriteProcessMemory(TargetProcess, reinterpret_cast<LPVOID>(address), &value, sizeof(T), nullptr)) {
+			std::cerr << "Failed to write memory: " << GetLastError() << std::endl;
+			return false;
+		}
+		return true;
 	}
 
-	// Basic RPM wrapper, easier to use.
-	template <typename var>
-	var ReadMemory(DWORD_PTR Address) {
-		var value;
-		ReadProcessMemory(TargetProcess, (LPCVOID)Address, &value, sizeof(var), NULL);
+	// Reads a value from a memory address in the target process.
+	// Returns the read value.
+	template <typename T>
+	T ReadMemory(DWORD_PTR address) {
+		T value;
+		if (!ReadProcessMemory(TargetProcess, reinterpret_cast<LPCVOID>(address), &value, sizeof(T), nullptr)) {
+			std::cerr << "Failed to read memory: " << GetLastError() << std::endl;
+		}
 		return value;
 	}
 
-	// for comparing a region in memory, needed in finding a signature
-	bool MemoryCompare(const BYTE* bData, const BYTE* bMask, const char* szMask) {
-		for (; *szMask; ++szMask, ++bData, ++bMask) {
-			if (*szMask == 'x' && *bData != *bMask) {
+	// Compares a region in memory with a pattern and mask.
+	// Returns true if the pattern matches the memory region according to the mask.
+	bool MemoryCompare(const BYTE* data, const BYTE* pattern, const char* mask) {
+		for (; *mask; ++mask, ++data, ++pattern) {
+			if (*mask == 'x' && *data != *pattern) {
 				return false;
 			}
 		}
-		return (*szMask == NULL);
+		return (*mask == NULL);
 	}
 
-	// for finding a signature/pattern in memory of another process
+	// Finds a signature in the memory of the target process.
+	// Returns the address of the found signature, or NULL if it was not found.
 	DWORD_PTR FindSignature(DWORD_PTR start, DWORD size, const char* sig, const char* mask)
 	{
 		BYTE* data = new BYTE[size];
 		SIZE_T bytesRead;
 
-		ReadProcessMemory(TargetProcess, (LPVOID)start, data, size, &bytesRead);
+		if (!ReadProcessMemory(TargetProcess, reinterpret_cast<LPVOID>(start), data, size, &bytesRead)) {
+			std::cerr << "Failed to read memory: " << GetLastError() << std::endl;
+			delete[] data;
+			return NULL;
+		}
 
 		for (DWORD i = 0; i < size; i++)
 		{
 			if (MemoryCompare((const BYTE*)(data + i), (const BYTE*)sig, mask)) {
+				delete[] data;
 				return start + i;
 			}
 		}
--- src/SkyEngine.cpp
+++ src/SkyEngine.cpp
@@ -3,41 +3,54 @@
 #include <Windows.h>
 #include "Memory.h"
 
-int main()
-{	
-	SetConsoleTitle(L"SkyEngine");
-	printf("Developed by - WiNiFiX#0204 (Jul 2019)\n");
+const char* PROCESS_NAME = "felsong-64.exe";
 
-	Memory memory;
-	if (memory.GetProcess("Wow.exe"))
-	{
-		printf("WoW Process Id       : %i\n", memory.TargetId);
-
-		auto mod = memory.GetModule("Wow.exe");
-		printf("WoW Base Address     : 0x%llX\n", mod.BaseAddress);
+void HandleProcess(Memory& memory) {
+	auto mod = memory.GetModule(PROCESS_NAME);
+	if (mod.Size != 0) {
+		printf("Successfully got module: %s. Base Address: 0x%llX\n", PROCESS_NAME, mod.BaseAddress);
 
 		auto address = memory.FindSignature(mod.BaseAddress, mod.Size, "\x4C\x8B\x0D\x00\x00\x00\x00\x45\x33\xC0\x48\x8B\xCE", "xxx????xxxxxx");
-		printf("WoW Sig Address      : 0x%llX\n", address);
+		printf("Signature Address: 0x%llX\n", address);
 
-	    auto TaintedAddress = address + memory.ReadMemory<DWORD>(address + 0x3) + 0x7;
-		printf("Lua_TaintedPtrOffset : 0x%llX\n", TaintedAddress - mod.BaseAddress);  // will be values close to: 0x2CB8B88; //0x2C93B48; //0x2C94BA8;	
-
-		DWORD_PTR lastLuaTaintedPtr = 0;
-		long count = 0;
+		auto TaintedAddress = address + memory.ReadMemory<DWORD>(address + 0x3) + 0x7;
+		printf("Lua_TaintedPtrOffset : 0x%llX\n", TaintedAddress - mod.BaseAddress);
 
 		printf("Lua is now unlocked...\n");
 
 		while (true)
 		{
 			memory.WriteMemory<DWORD_PTR>(TaintedAddress, 0);
+
+			// Press 'End' key to break the loop
+			if (GetAsyncKeyState(VK_END) & 1)
+				break;
+
 			Sleep(1);
 		}
 	}
+	else {
+		printf("Failed to get module: %s\n", PROCESS_NAME);
+	}
+}
 
-	printf("Please launch wow then re-open this unlocker\n");
+int main()
+{
+	SetConsoleTitle(L"SkyEngine");
+	printf("Developed by - WiNiFiX#0204 (Jul 2019) - Modified by Alejolas (May 2023)\n");
 
-	for (auto c = 5; c > 0; c--)
-	{
+	Memory memory;
+	HANDLE processHandle = memory.GetProcess(PROCESS_NAME);
+
+	if (processHandle != nullptr) {
+		printf("Successfully hooked into process: %s with ID: %i\n", PROCESS_NAME, memory.TargetId);
+		HandleProcess(memory);
+	}
+	else {
+		printf("Failed to hook into process: %s. Please launch it then re-open this unlocker.\n", PROCESS_NAME);
+	}
+
+	for (auto c = 5; c > 0; c--) {
 		printf("Closing in %i\n", c);
 		Sleep(1000);
 	}
